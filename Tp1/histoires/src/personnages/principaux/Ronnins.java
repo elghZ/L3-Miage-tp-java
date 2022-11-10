92aeb04d118b7a4146111d542bd688806ab94d46
El Ghariaoui Zaid <elghariaoui.zaid@gmail.com>
2022-11-10 14:06:04 +0100

    tp1 init

---
+++ Tp1/histoires/src/personnages/principaux/Ronnins.java
@@ -0,0 +1,11 @@
+package personnages.principaux;
+
+
+public class Ronnins extends Humains {
+
+	public Ronnins(String nom, String boissonPrefere, int argent) {
+		super(nom, boissonPrefere, argent);
+		// TODO Auto-generated constructor stub
+	}
+
+}
